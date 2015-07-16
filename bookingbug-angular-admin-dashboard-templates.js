3fd5eebe851009b7d3096899f97cba49077d889e
Ed Clements <ed@bookingbug.com>
2015-07-16 09:09:30 +0100

    First commit

---
+++ bookingbug-angular-admin-dashboard-templates.js
@@ -0,0 +1,1 @@
+angular.module("BB").run(["$templateCache", function($templateCache) {$templateCache.put("resource_calendar_main.html","<div ui-calendar=\"options.calendar\" ng-model=\"eventSources\"\n  ng-if=\"eventSources\" calendar=\"resourceCalendar\"\n  class=\"resource-calendar\"></div>\n");}]);
