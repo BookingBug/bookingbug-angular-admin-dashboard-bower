a0fdca12f082258e71502f92584fc61e1e5aef3b
Luke Ferguson <luke.ferguson@me.com>
2015-07-17 18:21:29 +0100

    Update to version: 0.0.50

--- bookingbug-angular-admin-dashboard-templates.js
+++ bookingbug-angular-admin-dashboard-templates.js
@@ -1,1 +1,1 @@
-angular.module("BB").run(["$templateCache", function($templateCache) {$templateCache.put("resource_calendar_main.html","<div ui-calendar=\"options.calendar\" ng-model=\"eventSources\"\n  ng-if=\"eventSources\" calendar=\"resourceCalendar\"\n  class=\"resource-calendar\"></div>\n");}]);
+angular.module("BB").run(["$templateCache", function($templateCache) {$templateCache.put("resource_calendar_main.html","<div ui-calendar=\"options.calendar\" ng-model=\"eventSources\"\r\n  ng-if=\"eventSources\" calendar=\"resourceCalendar\"\r\n  class=\"resource-calendar\"></div>\r\n");}]);
