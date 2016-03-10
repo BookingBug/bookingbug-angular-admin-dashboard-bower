e306e0ed6f16b41a2ae8081bdd4d44d79610b0a8
Jack Sharville <jacksharville@gmail.com>
2016-03-10 19:23:36 +0000

    Update to version: 0.0.326

--- bookingbug-angular-admin-dashboard.js
+++ bookingbug-angular-admin-dashboard.js
@@ -616,11 +616,11 @@
                 end_date: end.format('YYYY-MM-DD')
               };
               return AdminBookingService.query(params).then(function(bookings) {
-                var b, _i, _len, _ref;
+                var b, i, len, ref;
                 $scope.loading = false;
-                _ref = bookings.items;
-                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                  b = _ref[_i];
+                ref = bookings.items;
+                for (i = 0, len = ref.length; i < len; i++) {
+                  b = ref[i];
                   b.resourceId = b.person_id;
                 }
                 $scope.bookings = bookings.items;
@@ -680,18 +680,18 @@
             return $scope.editBooking(event);
           },
           resourceRender: function(resource, resourceTDs, dataTDs) {
-            var dataTD, resourceTD, _i, _j, _len, _len1, _results;
-            for (_i = 0, _len = resourceTDs.length; _i < _len; _i++) {
-              resourceTD = resourceTDs[_i];
+            var dataTD, i, j, len, len1, resourceTD, results;
+            for (i = 0, len = resourceTDs.length; i < len; i++) {
+              resourceTD = resourceTDs[i];
               resourceTD.style.height = "44px";
               resourceTD.style.verticalAlign = "middle";
             }
-            _results = [];
-            for (_j = 0, _len1 = dataTDs.length; _j < _len1; _j++) {
-              dataTD = dataTDs[_j];
-              _results.push(dataTD.style.height = "44px");
+            results = [];
+            for (j = 0, len1 = dataTDs.length; j < len1; j++) {
+              dataTD = dataTDs[j];
+              results.push(dataTD.style.height = "44px");
             }
-            return _results;
+            return results;
           },
           eventRender: function(event, element) {
             var service;
@@ -705,10 +705,10 @@
             }
           },
           eventAfterRender: function(event, elements, view) {
-            var element, _i, _len;
+            var element, i, len;
             if (view.type === "timelineDay") {
-              for (_i = 0, _len = elements.length; _i < _len; _i++) {
-                element = elements[_i];
+              for (i = 0, len = elements.length; i < len; i++) {
+                element = elements[i];
                 element.style.height = "27px";
               }
             }
@@ -748,12 +748,12 @@
             company: company
           };
           return AdminPersonService.query(params).then(function(people) {
-            var p, _i, _len, _ref;
+            var i, len, p, ref;
             $scope.loading = false;
             $scope.people = _.sortBy(people, 'name');
-            _ref = $scope.people;
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              p = _ref[_i];
+            ref = $scope.people;
+            for (i = 0, len = ref.length; i < len; i++) {
+              p = ref[i];
               p.title = p.name;
             }
             uiCalendarConfig.calendars.resourceCalendar.fullCalendar('refetchEvents');
@@ -849,13 +849,13 @@
           return collection.$get('services').then(function(services) {
             var s;
             scope.services = (function() {
-              var _i, _len, _results;
-              _results = [];
-              for (_i = 0, _len = services.length; _i < _len; _i++) {
-                s = services[_i];
-                _results.push(new BBModel.Admin.Service(s));
+              var i, len, results;
+              results = [];
+              for (i = 0, len = services.length; i < len; i++) {
+                s = services[i];
+                results.push(new BBModel.Admin.Service(s));
               }
-              return _results;
+              return results;
             })();
             return ColorPalette.setColors(scope.services);
           });
