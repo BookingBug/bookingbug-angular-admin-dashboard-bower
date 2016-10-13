7c6fc78e5b6104f2c5fac34f20890d90136e1cdb
edclements <ed.clements@gmail.com>
2016-10-13 14:13:58 +0100

    Version v2.0.38

--- bookingbug-angular-admin-dashboard.js
+++ bookingbug-angular-admin-dashboard.js
@@ -4174,10 +4174,12 @@
   * @description
   * Controller for the logout page
    */
-  angular.module('BBAdminDashboard.logout.controllers').controller('LogoutPageCtrl', function(BBModel, $scope, $state, $sessionStorage) {
+  angular.module('BBAdminDashboard.logout.controllers').controller('LogoutPageCtrl', function(AdminSsoLogin, BBModel, $scope, $state, $sessionStorage) {
     BBModel.Admin.Login.$logout().then(function() {
       $sessionStorage.removeItem("user");
       $sessionStorage.removeItem("auth_token");
+      AdminSsoLogin.ssoToken = null;
+      AdminSsoLogin.companyId = null;
       $state.go('login', {}, {
         reload: true
       });
